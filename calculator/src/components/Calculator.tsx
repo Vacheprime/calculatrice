53d2ee838abb297c8eff7934edd31b25f9ce13a7
Vacheprime <danatmuradov500@gmail.com>
2025-09-16 22:23:01 -0400

    Components: Added event handlers and basic logic for the calculator buttons.

--- calculator/src/components/Calculator.tsx
+++ calculator/src/components/Calculator.tsx
@@ -4,42 +4,148 @@ import React, { Component } from 'react'
 import Button from './Button'
 import buttonStyles from './Button.module.css'
 import calculatorStyles from './Calculator.module.css'
-import CalculatorInput from './CalculatorInput'
+import { create, all, ConfigOptions, ResultSet } from 'mathjs'
 
-// Define the props for the calculator
+/**
+ * The accepted properties for the Calculator.
+ */
 type CalculatorProps = {
-    containerClassName?: string
+  containerClassName?: string
 }
 
-class Calculator extends Component<CalculatorProps> {
+type CalculatorState = {
+  inputExpression: string
+}
+
+
+const mathConfig: ConfigOptions = {
+  number: "BigNumber",
+}
+
+/**
+ * The Calculator component creates a fully usable calculator.
+ * 
+ * @component
+ * 
+ * @param {CalculatorProps} props - The properties for the calculator.
+ */
+class Calculator extends Component<CalculatorProps, CalculatorState> {
+  // Create the math instance used to eval expressions
+  math = create(all, mathConfig);
+
+  constructor(props: CalculatorProps) {
+    super(props)
+  
+    this.state = {
+       inputExpression: ''
+    }
+  }
+
+  /**
+   * Add a digit to the inputExpression.
+   * 
+   * @param num the digit to add.
+   */
+  addDigitToExpr = (digit: string) => {
+    this.setState((previousState) => {
+      const prevExpr = previousState.inputExpression
+      return {inputExpression: prevExpr + digit}
+    });
+  }
+
+  /**
+   * Add an operator to the inputExpression.
+   * 
+   * @param operator the operator to add.
+   */
+  addOperatorToExpr = (operator: string) => {
+    this.setState((previousState) => {
+      const prevExpr = previousState.inputExpression
+      return {inputExpression: prevExpr + operator}
+    });
+  }
+
+  /**
+   * Delete the last character entered.
+   */
+  deleteLastChar = () => {
+    this.setState((previousState) => ({
+      inputExpression: previousState.inputExpression.slice(0, -1)
+    }))
+  }
+
+
+  addDecimalPointToExpr = () => {
+    this.addCharacterToExpr(".");
+  }
+
+  /**
+   * Add a character to the expression by updating the state.
+   * 
+   * @param char the character to add.
+   */
+  addCharacterToExpr = (char: string) => {
+    this.setState((previousState) => {
+      return {inputExpression: previousState.inputExpression + char}
+    });
+  }
+
+  addParenthesisToExpr = (isOpening: boolean) => {
+    const char = isOpening ? "(" : ")"
+    this.addCharacterToExpr(char)
+  }
+
+  /**
+   * Calculate the result of the expression.
+   */
+  calculateResult = () => {
+    const result: ResultSet = this.math.evaluate(this.state.inputExpression)
+    this.setState({inputExpression: result.toString()})
+  }
+
+  /**
+   * Clear the current expression.
+   */
+  clearExpr = () => {
+    this.setState({inputExpression: ''})
+  }
+
   render() {
     // Destructure main calculator container classname
     const {containerClassName} = this.props;
 
     return (
       <div className={containerClassName}>
-        <CalculatorInput/>
+        <input className={calculatorStyles.calculatorInput} value={this.state.inputExpression} readOnly={true}></input>
         <div className={calculatorStyles.innerCalculatorPanel}>
-          <Button buttonText="+" onClick={() => {}} className={buttonStyles.functionButton}/>
-          <Button buttonText="-" onClick={() => {}} className={buttonStyles.functionButton}/>
-          <Button buttonText="×" onClick={() => {}} className={buttonStyles.functionButton}/>
-          <Button buttonText="÷" onClick={() => {}} className={buttonStyles.functionButton}/>
-          <Button buttonText="7" onClick={() => {}} className={buttonStyles.inputButton}/>
-          <Button buttonText="8" onClick={() => {}} className={buttonStyles.inputButton}/>
-          <Button buttonText="9" onClick={() => {}} className={buttonStyles.inputButton}/>
-          <Button buttonText="AC" onClick={() => {}} className={buttonStyles.functionButton}/>
-          <Button buttonText="4" onClick={() => {}} className={buttonStyles.inputButton}/>
-          <Button buttonText="5" onClick={() => {}} className={buttonStyles.inputButton}/>
-          <Button buttonText="6" onClick={() => {}} className={buttonStyles.inputButton}/>
-          <Button buttonText="±" onClick={() => {}} className={buttonStyles.functionButton}/>
-          <Button buttonText="1" onClick={() => {}} className={buttonStyles.inputButton}/>
-          <Button buttonText="2" onClick={() => {}} className={buttonStyles.inputButton}/>
-          <Button buttonText="3" onClick={() => {}} className={buttonStyles.inputButton}/>
-          <Button buttonText="." onClick={() => {}} className={buttonStyles.functionButton}/>
-          <Button buttonText="0" onClick={() => {}} className={buttonStyles.inputButton}/>
-          <Button buttonText="(" onClick={() => {}} className={buttonStyles.functionButton}/>
-          <Button buttonText=")" onClick={() => {}} className={buttonStyles.functionButton}/>
-          <Button buttonText="=" onClick={() => {}} className={buttonStyles.equalsButton}/>
+          <Button buttonText="+" onClick={() => {this.addOperatorToExpr("+")}} className={buttonStyles.functionButton}/>
+          <Button buttonText="-" onClick={() => {this.addOperatorToExpr("-")}} className={buttonStyles.functionButton}/>
+          <Button buttonText="×" onClick={() => {this.addOperatorToExpr("×")}} className={buttonStyles.functionButton}/>
+          <Button buttonText="÷" onClick={() => {this.addOperatorToExpr("÷")}} className={buttonStyles.functionButton}/>
+            
+          <Button buttonText="7" onClick={() => {this.addDigitToExpr("7")}} className={buttonStyles.inputButton}/>
+          <Button buttonText="8" onClick={() => {this.addDigitToExpr("8")}} className={buttonStyles.inputButton}/>
+          <Button buttonText="9" onClick={() => {this.addDigitToExpr("9")}} className={buttonStyles.inputButton}/>
+          
+          <Button buttonText="AC" onClick={() => {this.clearExpr()}} className={buttonStyles.functionButton}/>
+          
+          <Button buttonText="4" onClick={() => {this.addDigitToExpr("4")}} className={buttonStyles.inputButton}/>
+          <Button buttonText="5" onClick={() => {this.addDigitToExpr("5")}} className={buttonStyles.inputButton}/>
+          <Button buttonText="6" onClick={() => {this.addDigitToExpr("6")}} className={buttonStyles.inputButton}/>
+          
+          <Button buttonText="DEL" onClick={this.deleteLastChar} className={buttonStyles.functionButton}/>
+          
+          <Button buttonText="1" onClick={() => {this.addDigitToExpr("1")}} className={buttonStyles.inputButton}/>
+          <Button buttonText="2" onClick={() => {this.addDigitToExpr("2")}} className={buttonStyles.inputButton}/>
+          <Button buttonText="3" onClick={() => {this.addDigitToExpr("3")}} className={buttonStyles.inputButton}/>
+          
+          <Button buttonText="." onClick={this.addDecimalPointToExpr} className={buttonStyles.functionButton}/>
+          
+          <Button buttonText="0" onClick={() => {this.addDigitToExpr("0")}} className={buttonStyles.inputButton}/>
+          
+          <Button buttonText="(" onClick={() => {this.addParenthesisToExpr(true)}} className={buttonStyles.functionButton}/>
+          <Button buttonText=")" onClick={() => {this.addParenthesisToExpr(false)}} className={buttonStyles.functionButton}/>
+          <Button buttonText="=" onClick={this.calculateResult} className={buttonStyles.equalsButton}/>
         </div>
       </div>
     )
