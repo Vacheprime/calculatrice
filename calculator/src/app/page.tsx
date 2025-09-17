43a30d30c3b73e4dec27341226b6584bfa4aab86
Vacheprime <danatmuradov500@gmail.com>
2025-09-16 20:45:42 -0400

    JSX: Adjusted the main page to use the Calculator component.

--- calculator/src/app/page.tsx
+++ calculator/src/app/page.tsx
@@ -1,39 +1,10 @@
-"use client"
-
-import CalculatorInput from "@/components/CalculatorInput";
+import Calculator from "@/components/Calculator";
 import pageStyles from "./page.module.css";
-import Button from "@/components/Button";
-import buttonStyles from '@/components/Button.module.css'
 
 export default function Home() {
   return (
     <main className={pageStyles.mainContentContainer}>
-      <div className={pageStyles.calculatorPanel}>
-        <CalculatorInput/>
-        <div className={pageStyles.innerCalculatorPanel}>
-          <Button buttonText="+" onClick={() => {}} className={buttonStyles.functionButton}/>
-          <Button buttonText="-" onClick={() => {}} className={buttonStyles.functionButton}/>
-          <Button buttonText="×" onClick={() => {}} className={buttonStyles.functionButton}/>
-          <Button buttonText="÷" onClick={() => {}} className={buttonStyles.functionButton}/>
-          <Button buttonText="7" onClick={() => {}} className={buttonStyles.inputButton}/>
-          <Button buttonText="8" onClick={() => {}} className={buttonStyles.inputButton}/>
-          <Button buttonText="9" onClick={() => {}} className={buttonStyles.inputButton}/>
-          <Button buttonText="AC" onClick={() => {}} className={buttonStyles.functionButton}/>
-          <Button buttonText="4" onClick={() => {}} className={buttonStyles.inputButton}/>
-          <Button buttonText="5" onClick={() => {}} className={buttonStyles.inputButton}/>
-          <Button buttonText="6" onClick={() => {}} className={buttonStyles.inputButton}/>
-          <Button buttonText="±" onClick={() => {}} className={buttonStyles.functionButton}/>
-          <Button buttonText="1" onClick={() => {}} className={buttonStyles.inputButton}/>
-          <Button buttonText="2" onClick={() => {}} className={buttonStyles.inputButton}/>
-          <Button buttonText="3" onClick={() => {}} className={buttonStyles.inputButton}/>
-          <Button buttonText="." onClick={() => {}} className={buttonStyles.functionButton}/>
-          <Button buttonText="0" onClick={() => {}} className={buttonStyles.inputButton}/>
-          <Button buttonText="(" onClick={() => {}} className={buttonStyles.functionButton}/>
-          <Button buttonText=")" onClick={() => {}} className={buttonStyles.functionButton}/>
-          <Button buttonText="=" onClick={() => {}} className={buttonStyles.equalsButton}/>
-        </div>
-        
-      </div>
+      <Calculator containerClassName={pageStyles.calculatorPanel}/>
     </main>
   );
 }
